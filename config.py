41a5f9cd7c98a94b16383e6eec06f121db45de31
sahil140708 <sahilarmy10@gmail.com>
2025-08-06 10:29:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,32 @@ from os import getenv
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-COOKIES = getenv("AUTH_CODE", "")
+COOKIES = getenv("AUTH_CODE", "# Netscape HTTP Cookie File
+# http://curl.haxx.se/rfc/cookie_spec.html
+# This is a generated file!  Do not edit.
+
+.oliveboard.in	TRUE	/	FALSE	1761831270	_gcl_au	1.1.922962104.1754055270
+.oliveboard.in	TRUE	/	FALSE	1789016234	_ga	GA1.1.412648769.1754055271
+.oliveboard.in	TRUE	/	FALSE	1762232248	_fbp	fb.1.1754055271763.715735708262152340
+.oliveboard.in	TRUE	/	FALSE	1763096181	susmid	da081dedMTI1MjEzNzVeXnNhaGlsYXJteTEwe47706ba
+.oliveboard.in	TRUE	/	FALSE	1763096181	uauth	da081ded958322c9ae515851e47706ba
+.oliveboard.in	TRUE	/	FALSE	1762715123	regauth	da081ded958322c9ae515851e47706ba
+.oliveboard.in	TRUE	/	FALSE	1762715123	obsession	VRHKN2UFOTExMjUyMTM3NQ==RKL8CRBX
+.oliveboard.in	TRUE	/	FALSE	1763096181	obwb	2000
+.oliveboard.in	TRUE	/	FALSE	1763096181	obph	9817614995%2C+Regd+0+days+ago%2C+prem+%3D+None
+.oliveboard.in	TRUE	/	FALSE	1763096181	obem	sahilarmy10%40gmail.com
+.oliveboard.in	TRUE	/	FALSE	1788615355	obctapinfo3	1
+.oliveboard.in	TRUE	/	FALSE	1755770234	lcatv	ssc
+.oliveboard.in	TRUE	/	FALSE	1789016217	WZRK_G	7fdcd2c06c434ddeae5e650e5a03fab9
+.oliveboard.in	TRUE	/	FALSE	1754459780	offerimg	1
+.oliveboard.in	TRUE	/	FALSE	1754457981	pupnt	0
+.oliveboard.in	TRUE	/	FALSE	1789016234	__utma	94392130.1258055620.1754055270.1754059940.1754456181.3
+.oliveboard.in	TRUE	/	FALSE	0	__utmc	94392130
+.oliveboard.in	TRUE	/	FALSE	1770224233	__utmz	94392130.1754456181.3.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)
+.oliveboard.in	TRUE	/	FALSE	1754456780	__utmt	1
+.oliveboard.in	TRUE	/	FALSE	1754457417	WZRK_S_65R-6K5-785Z	%7B%22p%22%3A2%2C%22s%22%3A1754456183%2C%22t%22%3A1754456217%7D
+.oliveboard.in	TRUE	/	FALSE	1763096234	oblpv	https%253A%252F%252Fu1.oliveboard.in%252Fexams%252F%253Fc%253Dssc2019%2526i%253Dssc
+.oliveboard.in	TRUE	/	FALSE	1754458033	__utmb	94392130.4.10.1754456181
+.oliveboard.in	TRUE	/	FALSE	1789016234	_ga_M8DH7WJ9QL	GS2.1.s1754456181$o3$g1$t1754456234$j7$l0$h0
+")
 OWNER_ID = getenv("OWNER_ID", "1213121") # if want to make accessible in channel put channel id in owner id field
